23898f0443ee384f6c2badf10a668c3c06587ba6
VaEscoCesar <thecesarvargas@gmail.com>
2020-06-27 17:22:24 -0500

    Creando la clase Memorama y su constructor

---
+++ js/script.js
@@ -0,0 +1,15 @@
+class Memorama {
+
+
+  constructor() {
+    this.totalTargeta = [];
+    this.numeroTarjeta = 0;
+    this.varificadorTarjeta = [];
+    this.errores = 0;
+    this.nivelDificultad = '';
+    this.imagenesCorrectas = [];
+    this.agregadorTarjetas = [];
+  }
+}
+
+new Memorama();
